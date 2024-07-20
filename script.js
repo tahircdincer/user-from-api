646262ef182e49934f7157237196886a8da74a08
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-07-20 15:42:31 +0300

    ' there was  console.log to see data on console. ' removed ' it '

--- script.js
+++ script.js
@@ -10,12 +10,6 @@ function fetchUser() {
     });
 }
 
-fetch("https://randomuser.me/api/")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.results[0]);
-  });
-
 function displayUser(user) {
   if (user.gender === "female") {
     document.body.style.backgroundColor = "pink";
